a60f7de1fe638fa6efecdeaad0a50a7114ad6fdf
Juraj Piovar <jpiovar@sk.ibm.com>
2019-12-17 20:15:42 +0100

    props

--- simple-redux/src/components/hi/Hi.tsx
+++ simple-redux/src/components/hi/Hi.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import './Hi.scss';
 
-
-export const Hi = () => {
+export const Hi = (props: any) => {
   const {count, text} = useSelector((state: any) => state);
 
   return (

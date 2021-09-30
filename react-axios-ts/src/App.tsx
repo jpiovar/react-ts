db7ac3fafa6e01a256f4fdf321dd242fdd39bd79
Juraj Piovar <jpiovar@sk.ibm.com>
2021-09-30 13:55:35 +0200

    react axios initial

---
+++ react-axios-ts/src/App.tsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return <div>Welcome<br/>
+    <button type="button" className="btn btn-primary">bootstrap button</button>
+    </div>;
+  }
+}
+
+export default App;

5a2ac1b6707936cb57bd0ebc336f78f1d491e415
Juraj Piovar <jpiovar@sk.ibm.com>
2021-10-13 15:12:45 +0200

    table with dispatch useSelector store via funtional components in class components

--- react-redux-axios-thunk/src/containers/records/components/table.tsx
+++ react-redux-axios-thunk/src/containers/records/components/table.tsx
@@ -1,10 +1,51 @@
 import { Component } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { TableItem } from "../../../redux/types/action-types-table";
 
+const TableAllItems = () => {
+    const items = useSelector((state: any) => state.allTableItems.items);
+
+    console.log("Items: ", items);
+
+    const renderList = items.map((item: TableItem, index: number) => {
+        const { id, firstname, surname, age } = item;
+        return (
+            <tr key={index}>
+                <td>{id}</td>
+                <td>{firstname}</td>
+                <td>{surname}</td>
+                <td>{age}</td>
+                <td><Link to={`/records/${id}`}>Click</Link></td>
+            </tr>
+        );
+    });
+
+    return (
+        <>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Firstname</th>
+                        <th>Surname</th>
+                        <th>Age</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {renderList}
+                </tbody>
+            </table>
+        </>
+    );
+};
 class TableComponent extends Component {
     render() {
         return (
             <div>
                 table component
+                <TableAllItems />
             </div>
         )
     }
--- react-redux-axios-thunk/src/containers/records/recordsPage.tsx
+++ react-redux-axios-thunk/src/containers/records/recordsPage.tsx
@@ -1,51 +1,53 @@
 import { Component, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { fetchTableAll } from "../../redux/actions/tableActions";
 import TableComponent from "./components/table";
 
-const TableAll = () => {
-    const items = useSelector((state) => state);
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(fetchTableAll());
-    }, [dispatch]);
-    console.log("Table items: ", items);
-    return (
-        <div className="ui grid container">
-            <TableComponent />
-        </div>
-    );
-};
-
-// function TableAllItems() { // Rule 2: call hooks in function component
-//     debugger;
-//     const items = useSelector((state: any) => state.allTableItems.items); // Rule 1: call hooks in top-level
+// const TableAll = () => {
+//     const items = useSelector((state) => state);
 //     const dispatch = useDispatch();
 
 //     useEffect(() => {
-//         debugger;
 //         dispatch(fetchTableAll());
-//         console.log("Table all items: ", items);
-//     }, [dispatch, items]);
+//     }, [dispatch]);
+//     console.log("Table items: ", items);
+//     return (
+//         <div className="ui grid container">
+//             <TableComponent />
+//         </div>
+//     );
+// };
 
+function TableAllItemsFetch() { // Rule 2: call hooks in function component
+    debugger;
+    // const items = useSelector((state: any) => state.allTableItems.items); // Rule 1: call hooks in top-level
+    const dispatch = useDispatch();
 
-//     return <>{items}</>
-// }
+    useEffect(() => {
+        debugger;
+        dispatch(fetchTableAll());
+        // console.log("Table all items: ", items);
+    }, [dispatch]);
+    return <></>
+}
 
 class RecordsPage extends Component {
 
-    renderTable() {
-        debugger;
-        // return <TableAllItems />
-        return <TableAll />;
-    }
+    // renderTable() {
+    //     debugger;
+    //     // return <TableAllItems />
+    //     return <TableAll />;
+    // }
 
     render() {
         debugger;
         return (
             // <>{this.renderTable()}</>
-            <TableAll/>
+            // <TableAll/>
+            <>
+            <TableAllItemsFetch/>
+            <TableComponent/>
+            </>
         )
     }
 }
